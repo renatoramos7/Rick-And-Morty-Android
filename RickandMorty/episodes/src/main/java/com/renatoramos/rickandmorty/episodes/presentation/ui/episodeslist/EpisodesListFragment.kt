ad41d1ad8816a65e07cd9287ca7b948be918ee77
Renato Ramos <ckguitar44@gmail.com>
2020-02-18 15:57:45 -0300

    Partial development. Modularization. Navigation Configuration.

--- RickandMorty/episodes/src/main/java/com/renatoramos/rickandmorty/episodes/presentation/ui/episodeslist/EpisodesListFragment.kt
+++ RickandMorty/episodes/src/main/java/com/renatoramos/rickandmorty/episodes/presentation/ui/episodeslist/EpisodesListFragment.kt
@@ -1,13 +1,10 @@
 package com.renatoramos.rickandmorty.episodes.presentation.ui.episodeslist
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.renatoramos.rickandmorty.episodes.R
 
 class EpisodesListFragment : Fragment() {
